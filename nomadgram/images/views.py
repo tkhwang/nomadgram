e20bd3cdeeeefec8fa737eebeb703fa226070dd9
tkhwang <tkhwang@gmail.com>
2017-11-15 18:34:43 +0900

    [+] Nomadgram django

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 # Create your views here.
@@ -35,10 +36,29 @@ def get_key(image):
 
 class LikeImage(APIView):
     
-    def get(self, request, image_id, format=None):
-        
-        print(image_id)
-        
-        return Response(status=200)
+    def post(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image 
+            )
+            preexisting_like.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image 
+            )
+            new_like.save()
+            return Response(status=status.HTTP_201_CREATED) 
 
  
